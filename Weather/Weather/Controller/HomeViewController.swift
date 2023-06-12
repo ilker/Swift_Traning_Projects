f7b5479b91df4f3758f781c066a28e2feaefeee1
Ilker Tacyildiz <tacyildizilker@gmail.com>
2023-06-13 02:13:49 +0300

    auto layout (programmatically)

--- Weather/Weather/Controller/HomeViewController.swift
+++ Weather/Weather/Controller/HomeViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 class HomeViewController: UIViewController {
     // MARK: - Properties
     private let backgroundImageView = UIImageView()
+    private let mainStackView = UIStackView()
+    private let searchStackView = SearchStackView()
+    private let statusImageView = UIImageView()
+    private let temperatureLabel = UILabel()
+    private let cityLabel = UILabel()
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +29,60 @@ extension HomeViewController {
         backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
         backgroundImageView.contentMode = .scaleAspectFill
         backgroundImageView.image = UIImage(named: "background")
+        //searchStackView style
+        searchStackView.translatesAutoresizingMaskIntoConstraints = false
+        searchStackView.spacing = 8
+        searchStackView.axis = .horizontal
+        //mainStackView style
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.spacing = 10
+        mainStackView.axis = .vertical
+        mainStackView.alignment = .trailing
+        //statusImageView style
+        statusImageView.translatesAutoresizingMaskIntoConstraints = false
+        statusImageView.image = UIImage(systemName: "sun.max")
+        statusImageView.tintColor = .label
+        //temparetureLabel style
+        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        temperatureLabel.font = UIFont.systemFont(ofSize: 80)
+        temperatureLabel.attributedText = attributedText(with: "15")
+        //cityLabel style
+        cityLabel.translatesAutoresizingMaskIntoConstraints = false
+        cityLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        cityLabel.text = "Hatay"
     }
     private func layout()  {
         view.addSubview(backgroundImageView)
+        view.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(searchStackView)
+        mainStackView.addArrangedSubview(statusImageView)
+        mainStackView.addArrangedSubview(temperatureLabel)
+        mainStackView.addArrangedSubview(cityLabel)
         NSLayoutConstraint.activate([
+            //backgroundImageView layout
             backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
             backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             view.trailingAnchor.constraint(equalTo: backgroundImageView.trailingAnchor),
-            view.bottomAnchor.constraint(equalTo: backgroundImageView.bottomAnchor)
+            view.bottomAnchor.constraint(equalTo: backgroundImageView.bottomAnchor),
+            
+            //StackView layout
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            
+            searchStackView.widthAnchor.constraint(equalTo: mainStackView.widthAnchor),
+            
+            //statusImageView layout
+            statusImageView.heightAnchor.constraint(equalToConstant: 85),
+            statusImageView.widthAnchor.constraint(equalToConstant: 85)
+            
         ])
+    }
+    
+    private func attributedText(with text: String) -> NSMutableAttributedString {
+        let attributedText = NSMutableAttributedString(string: text, attributes: [.foregroundColor : UIColor.label, .font : UIFont.boldSystemFont(ofSize: 90)])
+        attributedText.append(NSAttributedString(string: "°C", attributes: [.font : UIFont.systemFont(ofSize: 60)]))
+        return attributedText
     }
 
 }
---
+++ Weather/Weather/View/SearchStackView.swift
@@ -0,0 +1,66 @@
+//
+//  SearchStackView.swift
+//  Weather
+//
+//  Created by ILKER on 13.06.2023.
+//
+
+import UIKit
+
+class SearchStackView: UIStackView {
+    // MARK: - Properties
+    private let locationButton = UIButton(type: .system)
+    private let searchButton = UIButton(type: .system)
+    private let searchTextField = UITextField()
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        style()
+        layout()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+// MARK: - Helpers
+extension SearchStackView {
+    private func style() {
+        //locationButton style
+        locationButton.translatesAutoresizingMaskIntoConstraints = false
+        locationButton.setImage(UIImage(systemName: "location.circle.fill"), for: .normal)
+        locationButton.tintColor = .label
+        locationButton.layer.cornerRadius = 40 / 2
+        locationButton.contentVerticalAlignment = .fill
+        locationButton.contentHorizontalAlignment = .fill
+        //searchButton style
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchButton.tintColor = .label
+        searchButton.layer.cornerRadius = 40 / 2
+        searchButton.contentVerticalAlignment = .fill
+        searchButton.contentHorizontalAlignment = .fill
+        //searchTextField style
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchTextField.placeholder = "Search..."
+        searchTextField.font = UIFont.preferredFont(forTextStyle: .title1)
+        searchTextField.borderStyle = .roundedRect
+        searchTextField.textAlignment = .center
+        searchTextField.backgroundColor = .systemFill
+    }
+    private func layout() {
+        addArrangedSubview(locationButton)
+        addArrangedSubview(searchTextField)
+        addArrangedSubview(searchButton)
+        
+        NSLayoutConstraint.activate([
+            //locationButton layout
+            locationButton.heightAnchor.constraint(equalToConstant: 40),
+            locationButton.widthAnchor.constraint(equalToConstant: 40),
+            
+            //searhButton layout
+            searchButton.heightAnchor.constraint(equalToConstant: 40),
+            searchButton.widthAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+}
