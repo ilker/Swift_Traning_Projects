8e737019f0a25958f5647c94a3bd38d4258357a4
Ilker Tacyildiz <tacyildizilker@gmail.com>
2023-06-16 17:39:59 +0300

    UniversityInformation

---
+++ UniversityInformation/UniversityInformation/Controller/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UniversityInformation
+//
+//  Created by ILKER on 16.06.2023.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    // MARK: - Properties
+    var viewModel = MainViewModel()
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    private func configureView() {
+        
+    }
+    func setup() {
+        configureView()
+        registerTableView()
+    }
+
+}
+// MARK: - Helpers
---
+++ UniversityInformation/UniversityInformation/MainViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewModel.swift
+//  UniversityInformation
+//
+//  Created by ILKER on 16.06.2023.
+//
+
+import Foundation
+
+class MainViewModel {
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        return 10
+    }
+    func numberOfSection() -> Int {
+        return 1
+    }
+}
