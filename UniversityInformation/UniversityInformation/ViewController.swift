beb640782aeba90ee3259ce771298fe960837428
Ilker Tacyildiz <tacyildizilker@gmail.com>
2023-06-16 14:29:31 +0300

    UniversityInformation

---
+++ UniversityInformation/UniversityInformation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniversityInformation
+//
+//  Created by ILKER on 16.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
