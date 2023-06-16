7583e2466546214d32410d8e8e8db76a79cb7299
Ilker Tacyildiz <tacyildizilker@gmail.com>
2023-06-16 15:13:01 +0300

    UniversityInformatin

---
+++ UniversityInformation/UniversityInformation/Controller/MainViewController+TableView.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController+TableView.swift
+//  UniversityInformation
+//
+//  Created by ILKER on 16.06.2023.
+//
+
+import UIKit
+private let reuseIdentifier = "cell"
+extension MainViewController {
+    func registerTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.numberOfSection()
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.numberOfRowsInSection(section)
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
---
+++ UniversityInformation/UniversityInformation/ViewModel/MainViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewModel.swift
+//  UniversityInformation
+//
+//  Created by ILKER on 16.06.2023.
+//
+
+import Foundation
+
+class MainViewModel {
+    func numberOfSection() -> Int{
+        return 1
+    }
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        return 10
+    }
+}
