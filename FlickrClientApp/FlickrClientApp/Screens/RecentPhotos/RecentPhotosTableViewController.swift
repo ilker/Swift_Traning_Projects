1c3a85b632ef3b02310b9d9518cb3395efa80539
Ilker Tacyildiz <tacyildizilker@gmail.com>
2023-06-26 23:01:56 +0300

    FlickrClientApp

---
+++ FlickrClientApp/FlickrClientApp/Screens/RecentPhotos/RecentPhotosTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrClientApp
+//
+//  Created by ILKER on 26.06.2023.
+//
+
+import UIKit
+
+class RecentPhotosTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
