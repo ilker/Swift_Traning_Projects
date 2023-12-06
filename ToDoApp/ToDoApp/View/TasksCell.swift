5d49c47f49f9e5f5f6e24ae182348ca09a756b80
ilker tacyildiz <tacyildizilker@gmail.com>
2023-12-06 13:30:31 +0300

    ToDoApp
    CollectionView Created

---
+++ ToDoApp/ToDoApp/View/TasksCell.swift
@@ -0,0 +1,19 @@
+//
+//  TasksCell.swift
+//  ToDoApp
+//
+//  Created by ILKER on 6.12.2023.
+//
+
+import UIKit
+
+class TasksCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .brown
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
