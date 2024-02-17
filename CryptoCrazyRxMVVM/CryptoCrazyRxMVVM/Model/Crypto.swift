ab614430a663149b704b208776d526c7247ea3d6
ilker tacyildiz <48527920+ilker@users.noreply.github.com>
2024-02-17 19:05:13 +0300

    CryptoCrazy+RxSwift+MVVM

---
+++ CryptoCrazyRxMVVM/CryptoCrazyRxMVVM/Model/Crypto.swift
@@ -0,0 +1,14 @@
+//
+//  Crypto.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by ILKER on 17.02.2024.
+//
+
+import Foundation
+
+struct Crypto: Decodable {
+    let currency: String
+    let price: String
+}
+
