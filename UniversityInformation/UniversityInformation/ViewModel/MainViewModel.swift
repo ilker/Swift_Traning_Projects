722d07cc60c6a97c43d9466d38f9612a476679db
ilker tacyildiz <tacyildizilker@gmail.com>
2024-12-15 13:06:16 +0300

    UniversityInformation
    TableView created.

---
+++ UniversityInformation/UniversityInformation/ViewModel/MainViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewModel.swift
+//  UniversityInformation
+//
+//  Created by ILKER on 15.12.2024.
+//
+
+import Foundation
+
+class MainViewModel {
+    func numberOfSection() -> Int {
+        return 1
+    }
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        return 10
+    }
+}
